3127fa20dc2126c89608f83c7ce95843f46c261b
LAPTOP-94U6BQOF\Asus <tegarsh1@gmail.com>
2022-08-13 21:33:23 +0700

    message

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-$receiving_email_address = 'https://formsubmit.co/6bffdfb39218023aefe0e83bf3ac59c5';
+$receiving_email_address = 'tegarsh1@gmail.com';
 if( file_exists($php_email_form = '../assets/vendor/php-email-form/validate.js' )) {
   include( $php_email_form );
 } else {
@@ -11,8 +11,15 @@ $contact->to = $receiving_email_address;
 $contact->from_name = $_POST['name'];
 $contact->from_email = $_POST['email'];
 $contact->subject = $_POST['subject'];
+$contact->smtp = array(
+  'host' => 'smtp.elasticemail.com',
+  'username' => 'tegarsh1@gmail.com',
+  'password' => 'CCB0CEC367635CB3950D591428C6F0DD064E',
+  'port' => '2525'
+  );
 $contact->add_message( $_POST['name'], 'From');
 $contact->add_message( $_POST['email'], 'Email');
 $contact->add_message( $_POST['message'], 'Message', 10);
+$contact->honeypot = $_POST['submit'];
 echo $contact->send();
 ?>
